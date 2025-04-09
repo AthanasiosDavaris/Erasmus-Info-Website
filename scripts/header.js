78a9441ed821946c5833c34db072ca3d19b279da
Thanos Davaris <davthn08@gmail.com>
2025-04-09 21:11:57 +0300

    Finished Header and started index

--- scripts/header.js
+++ scripts/header.js
@@ -1,4 +1,6 @@
 function toggleMenu() {
   const navbar = document.querySelector('.navbar');
+  const hamburger = document.querySelector('.dropdown');
   navbar.classList.toggle('active');
+  hamburger.classList.toggle('active');
 }
