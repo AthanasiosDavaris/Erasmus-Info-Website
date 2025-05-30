ff696ba7600a4156bdbf3b80b9fe7efa4dce3fad
Thanos Davaris <davthn08@gmail.com>
2025-05-30 13:23:29 +0300

    added basic sign-up (needs improvement)

--- html/sign-up.php
+++ html/sign-up.php
@@ -7,12 +7,14 @@ $email = $_POST["email"];
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-echo $first_name;
-echo $last_name;
-echo $student_id;
-echo $phone;
-echo $email;
-echo $username;
-echo $password;
+$con = mysqli_connect("localhost", "root", "");
+
+if(!$con) {
+  echo "problem in the connection".mysqli_error();
+} else {
+  mysqli_select_db($con, "erasm");
+  mysqli_query($con, "INSERT INTO users (first_name, last_name, student_id, phone, email, username, password) VALUES ('$first_name', '$last_name', '$student_id', '$phone', '$email', '$username', '$password')");
+}
+mysqli_close($con);
 
 ?>
